3db9dd509ce4bcf91dcbc3ef6930ffa0ca6c5341
asahmed <asar0786@gmail.com>
2020-10-03 12:28:22 +0200

    Updated

--- compare_SM_with_RwgtSM_DiffPars_NanoAOD_Selection.py
+++ compare_SM_with_RwgtSM_DiffPars_NanoAOD_Selection.py
@@ -92,40 +92,43 @@ def createCanvasPads():
 
 #leg = r.TLegend(0.6,0.65,0.9,0.9)
 leg = r.TLegend(0.1,0.1,0.4,0.3)
-
+file_SM = "/eos/cms/store/user/jlawhorn/WVJJTree_Jul20/2018/WplusToLNuWplusTo2JJJ_EWK_LO_SM_MJJ100PTJ10_TuneCP5_13TeV-madgraph-pythia8.root"
+file_aQGC = "/eos/cms/store/user/jlawhorn/WVJJTree_Jul20/2018/WplusTo2LWplusTo2JJJ_EWK_LO_aQGC_MJJ100PTJ10_TuneCP5_13TeV-madgraph-pythia8.root"
+lumi = "59.0"
+X_SM = "0.08793"
+X_aQGC = "3.451"
+process = "WplusToLNuWplusTo2JJJ"
 # Read three fils and their tree
-tfile_OnlyFT1_initialize = r.TFile("/eos/cms/store/user/jlawhorn/WVJJTree_Jun24/2018_aQGC/WminusTo2JZTo2LJJ_EWK_LO_aQGC_MJJ100PTJ10_TuneCP5_13TeV-madgraph-pythia8.root")
+tfile_OnlyFT1_initialize = r.TFile(file_aQGC)
+#tfile_OnlyFT1_initialize = r.TFile("/eos/cms/store/user/jlawhorn/WVJJTree_Jun24/2018_aQGC/WminusTo2JZTo2LJJ_EWK_LO_aQGC_MJJ100PTJ10_TuneCP5_13TeV-madgraph-pythia8.root")
 #tfile_OnlyFT1_initialize = r.TFile("/eos/cms/store/user/jlawhorn/WVJJTree_Jun24/2018_aQGC/WplusTo2JWminusTo2LJJ_EWK_LO_aQGC_MJJ100PTJ10_TuneCP5_13TeV-madgraph-pythia8.root")
-tfile_SM = r.TFile("/eos/cms/store/user/jlawhorn/WVJJTree_Apr20/2016/WminusTo2JZTo2LJJ_EWK_LO_SM_MJJ100PTJ10_TuneCUETP8M1_13TeV-madgraph-pythia8.root")
+tfile_SM = r.TFile(file_SM)
+#tfile_SM = r.TFile("/eos/cms/store/user/jlawhorn/WVJJTree_Apr20/2016/WminusTo2JZTo2LJJ_EWK_LO_SM_MJJ100PTJ10_TuneCUETP8M1_13TeV-madgraph-pythia8.root")
 #tfile_SM = r.TFile("/eos/cms/store/user/jlawhorn/WVJJTree_Jun20/2017/WplusTo2JWminusToLNuJJ_EWK_LO_SM_MJJ100PTJ10_TuneCP5_13TeV-madgraph-pythia8.root")
 
 tree_OnlyFT1_initialize = tfile_OnlyFT1_initialize.Get("Events")
-#tree_AllaQGCPar_initialize = tfile_AllaQGCPar_initialize.Get("Events")
 tree_SM = tfile_SM.Get("Events")
-#print tree_SM.vbf_pt
+n_aQGC = tree_OnlyFT1_initialize.GetEntries()
+n_SM = tree_SM.GetEntries()
+print n_SM
+print n_aQGC
 # See which weights we are going to apply
 for n, event in enumerate(tree_OnlyFT1_initialize):
 	if n>1:
 		break
 	print "Weight Details for OnlyFT1_initialize: ID = ",event.aqgcWeight[35],"\tWeight = ",event.aqgcWeight[35]
-#for n, event in enumerate(tree_AllaQGCPar_initialize):
-#	if n>0:
-#		break
-#	print "Weight Details for AllaQGCPar_initialize: ID = ",event.LHEWeightIDs[491],"\tWeight = ",event.aqgcWeight[491]
 
-
+#print X_SM*lumi
 # Define 3 histogram for three different samples
 ## Hist 1
 print "Reading Histo: h_OnlyFT1_initialize..."
-#h_OnlyFT1_initialize = r.TH1F("h_OnlyFT1_initialize","",20,args.xmin,args.xmax)
 h_OnlyFT1_initialize = r.TH1F("h_OnlyFT1_initialize","",args.nbin,args.xmin,args.xmax)
 h_OnlyFT1_initialize.GetXaxis().SetTitle("mT (WW) GeV")
 ylabel = "Events / %s GeV" % int(h_OnlyFT1_initialize.GetBinWidth(1))
 h_OnlyFT1_initialize.GetYaxis().SetTitle(ylabel)
 h_OnlyFT1_initialize.SetLineColor(1)
 h_OnlyFT1_initialize.SetMarkerColor(1)
-leg.AddEntry(h_OnlyFT1_initialize,"Reweighted SM","lpe")
-#leg.AddEntry(h_OnlyFT1_initialize,"Reweighted SM","lpe")
+leg.AddEntry(h_OnlyFT1_initialize,"aQGC FT1=1.0","lpe")
 
 ## Hist 2
 print "Reading Histo: h_aQGC_initialize..."
@@ -135,27 +138,9 @@ ylabel = "Events / %s GeV" % int(h_OnlyFT1_initialize.GetBinWidth(1))
 h_aQGC_initialize.GetYaxis().SetTitle(ylabel)
 h_aQGC_initialize.SetLineColor(2)
 h_aQGC_initialize.SetMarkerColor(2)
-leg.AddEntry(h_aQGC_initialize,"aQGC (FT1=1.0)","lpe")
-#leg.AddEntry(h_OnlyFT1_initialize,"Reweighted SM","lpe")
-
-# Hist 3
-#print "Reading Histo: h_FT5_12e12..."
-h_FT5_12e12 = r.TH1F("h_FT5_12e12","",args.nbin,args.xmin,args.xmax)
-h_FT5_12e12.GetXaxis().SetTitle("mT (WW) GeV")
-ylabel = "Events / %s GeV" % int(h_FT5_12e12.GetBinWidth(1))
-h_FT5_12e12.GetYaxis().SetTitle(ylabel)
-h_FT5_12e12.SetLineColor(4)
-h_FT5_12e12.SetMarkerColor(4)
-#leg.AddEntry(h_FT5_12e12,"aQGC (FT5=12.0)","lpe")
-#leg.AddEntry(h_FT0_14e12,"Reweighted SM","lpe")
+leg.AddEntry(h_aQGC_initialize,"Reweighted SM (FT1=0.0)","lpe")
 
 ## Hist 4
-#h_AllaQGCPar_initialize = r.TH1F("h_AllaQGCPar_initialize","",20,0,1500)
-#h_AllaQGCPar_initialize.SetLineColor(4)
-#h_AllaQGCPar_initialize.SetMarkerColor(4)
-#leg.AddEntry(h_AllaQGCPar_initialize,"Reweighted SM (Set all Par)","lpe")
-
-## Hist 5
 h_SM = r.TH1F("h_SM","",args.nbin,args.xmin,args.xmax)
 h_SM.Sumw2()
 h_SM.SetLineColor(5)
@@ -166,25 +151,23 @@ print "reading tree : 1"
 aQGC = args.tree_var+">>h_aQGC_initialize"
 Reweight = args.tree_var+">>h_OnlyFT1_initialize"
 SM = args.tree_var+">>h_SM"
-FT5 = args.tree_var+">>h_FT5_12e12"
+#FT5 = args.tree_var+">>h_FT5_12e12"
 #if(vbf1_AK4_pt > 50):
-tree_OnlyFT1_initialize.Draw(aQGC,"35.0*0.2223*aqgcWeight[25]*"+"("+args.cut+")","")
-tree_OnlyFT1_initialize.Draw(FT5,"35.0*0.2223*aqgcWeight[134]*"+"("+args.cut+")","")
-tree_OnlyFT1_initialize.Draw(Reweight,"35.0*0.2223*aqgcWeight[35]*"+"("+args.cut+")","")
-tree_SM.Draw(SM,"35.0*0.2982*"+"("+args.cut+")","")
+
+tree_OnlyFT1_initialize.Draw(Reweight,X_aQGC+"*"+lumi+"*aqgcWeight[26]/"+str(n_aQGC)+"*"+"("+args.cut+")","")
+tree_OnlyFT1_initialize.Draw(aQGC,X_aQGC+"*"+lumi+"*aqgcWeight[0]/"+str(n_aQGC)+"*"+"("+args.cut+")","")
+tree_SM.Draw(SM,X_SM+"*"+lumi+"/"+str(n_SM)+"*"+"("+args.cut+")","")
 #else:
 #print "condition not satisfied"
-#tree_SM.Draw("LHE_MothInfo_mtWLep>>h_SM","59.0*0.02982","")
-# Normalize histogram with unity
 
 #h_aQGC_initialize.Scale(1.0/h_aQGC_initialize.Integral())
 #h_FT5_12e12.Scale(1.0/h_FT5_12e12.Integral())
 #h_OnlyFT1_initialize.Scale(1.0/h_OnlyFT1_initialize.Integral())
 #h_SM.Scale(1.0/h_SM.Integral())
-
 #h_AllaQGCPar_initialize.Scale(1.0/h_AllaQGCPar_initialize.Integral())
 
-h3 = createRatio(h_SM,h_OnlyFT1_initialize,1)
+h3 = createRatio(h_SM,h_aQGC_initialize,1)
+#h3 = createRatio(h_SM,h_OnlyFT1_initialize,1)
 #h4 = createRatio(h_SM,h_AllaQGCPar_initialize,4)
 #h5 = createRatio(h_SM,h_FT0_8e12,2)
 
@@ -197,11 +180,11 @@ canvas, pad1, pad2 = createCanvasPads()
 # Draw all hitogram using sames.
 pad1.cd()
 h_OnlyFT1_initialize.Draw()
+h_SM.Draw("same")
 h_aQGC_initialize.Draw("same")
 #h_FT5_12e12.Draw("same")
 #h_AllaQGCPar_initialize.Draw("same")
 #h_FT0_10e12.Draw("same")
-h_SM.Draw("same")
 
 leg.Draw()
 
@@ -214,9 +197,7 @@ l.SetLineStyle(3)
 l.Draw("same")
 
 #canvas.Draw()
-canvas.SaveAs(args.tree_var+"_xsec_Nano.png")
-#canvas.SaveAs("LHE_MothInfo_mtWLep_xsec.png")
-#canvas.SaveAs("mt_plot_Comp_DifferentSM_Range1500_b250_log.pdf")
+canvas.SaveAs(args.tree_var+"_"+process+"_2018.png")
 
 clock.Stop()
 
